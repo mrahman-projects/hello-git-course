2c1af323c5ad476104378e199c8c161ceaf77e6a
Mijanur Rahman <bilashece@icloud.com>
2023-04-15 12:58:26 +0300

    added a third feature

--- hello_world.py
+++ hello_world.py
@@ -8,5 +8,15 @@ for i in range(1,10):
     print(numbers)
 
 print()
+
+print("this is the third feature added")
+while True:
+    user_input = input("guess a number, 0 to quit: ")
+    if user_input == '0':
+        break
+    else:
+        print(f"your input is: {user_input}")
+
+
 print("This is the end of the program")
 
