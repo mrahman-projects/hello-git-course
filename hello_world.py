9ec9805ed8f53f70088fab4d6cd2b30fbad92e6f
Mijanur Rahman <bilashece@icloud.com>
2023-04-15 12:38:51 +0300

    added the second feature

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,19 @@
 print("Hello World!")
 print()
 
+# this is the feature-01
 print("This is a new feature added")
 numbers = []
 for i in range(1,10):
     numbers.append(i)
     print(numbers)
+
+# this is the feature-02
+print()
+print("This is the second feature added")
+company_dict = {"1": "company 01", "2": "company 02", "3": "company 03", "4": "company 04"}
+for key, value in company_dict.items():
+    print(f"{key}: {value}")
 
 print()
 print("This is the end of the program")
